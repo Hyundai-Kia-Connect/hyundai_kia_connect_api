cf0795682922e91020018bed564a3042cfa3df7c
fuatakgun <fuatakgun@gmail.com>
2021-12-22 20:24:30 +0100

    extract API classes for testing purposes

--- hyundai_kia_connect_api/__init__.py
+++ hyundai_kia_connect_api/__init__.py
@@ -1,6 +1,10 @@
 """Top-level package for Hyundai / Kia Connect."""
 
 from .KiaUvoApiImpl import KiaUvoApiImpl
+from .HyundaiBlueLinkAPIUSA import HyundaiBlueLinkAPIUSA
+from .KiaUvoApiCA import KiaUvoApiCA
+from .KiaUvoApiEU import KiaUvoApiEU
+from .KiaUvoAPIUSA import KiaUvoAPIUSA
 from .Token import Token
 from .utils import get_implementation_by_region_brand
 from .Vehicle import Vehicle
