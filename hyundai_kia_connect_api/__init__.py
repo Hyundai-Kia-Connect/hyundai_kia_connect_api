21701755037277a72f1a39ba7f5829546eb4c09c
fuatakgun <fuatakgun@gmail.com>
2021-12-22 20:25:21 +0100

    chore: extract API classes

--- hyundai_kia_connect_api/__init__.py
+++ hyundai_kia_connect_api/__init__.py
@@ -5,6 +5,7 @@ from .HyundaiBlueLinkAPIUSA import HyundaiBlueLinkAPIUSA
 from .KiaUvoApiCA import KiaUvoApiCA
 from .KiaUvoApiEU import KiaUvoApiEU
 from .KiaUvoAPIUSA import KiaUvoAPIUSA
+
 from .Token import Token
 from .utils import get_implementation_by_region_brand
 from .Vehicle import Vehicle
