6b700ab2cf8e15f30583d3eb04a6659b7af34110
cdnninja <jaydenaphillips@gmail.com>
2021-12-24 23:02:12 -0700

    Update __init__.py

--- hyundai_kia_connect_api/__init__.py
+++ hyundai_kia_connect_api/__init__.py
@@ -7,6 +7,5 @@ from .KiaUvoApiEU import KiaUvoApiEU
 from .KiaUvoAPIUSA import KiaUvoAPIUSA
 
 from .Token import Token
-from .utils import get_implementation_by_region_brand
 from .Vehicle import Vehicle
 from .VehicleManager import VehicleManager
