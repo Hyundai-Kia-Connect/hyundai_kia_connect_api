eeb5cbad357535dd8726465fc87f2d4bc66b8571
cdnninja <jaydenaphillips@gmail.com>
2021-12-24 22:41:11 -0700

    fix: Error related to get_implementation

--- hyundai_kia_connect_api/VehicleManager.py
+++ hyundai_kia_connect_api/VehicleManager.py
@@ -48,3 +48,27 @@ class VehicleManager:
             entry.token = entry.api.login()
             return True
         return False
+    
+    def get_implementation_by_region_brand(
+    region: int,
+    brand: int,
+    username: str,
+    password: str,
+    pin: str = "",
+) -> KiaUvoApiImpl:  # pylint: disable=too-many-arguments
+    if REGIONS[region] == REGION_CANADA:
+        return KiaUvoApiCA(
+            username, password, region, brand, pin
+        )
+    elif REGIONS[region] == REGION_EUROPE:
+        return KiaUvoApiEU(
+            username, password, region, brand, pin
+        )
+    elif REGIONS[region] == REGION_USA and BRANDS[brand] == BRAND_HYUNDAI:
+        return HyundaiBlueLinkAPIUSA(
+            username, password, region, brand, pin
+        )
+    elif REGIONS[region] == REGION_USA and BRANDS[brand] == BRAND_KIA:
+        return KiaUvoAPIUSA(
+            username, password, region, brand, pin
+        )
