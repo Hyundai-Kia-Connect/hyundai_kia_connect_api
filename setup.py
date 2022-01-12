d4d6336ba9d4e504545689adf552feb8d1bc777a
Fuat Akgün <fuatakgun@gmail.com>
2022-01-12 19:09:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,11 @@ from setuptools import setup, find_packages
 
 with open("README.rst") as readme_file:
     readme = readme_file.read()
+    
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
 
+readme = ""
 history = ""
 
 requirements = ["curlify", "bs4", "push_receiver"]
