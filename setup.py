5fe8592ac7671133c3db36ec11212c5df602da06
cdnninja <cdnninja@users.noreply.github.com>
2022-01-15 22:27:50 +0000

    fix: black action

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from setuptools import setup, find_packages
 
 with open("README.rst") as readme_file:
     readme = readme_file.read()
-    
+
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-long_description=readme + "\n\n" + history
-long_description=""
+long_description = readme + "\n\n" + history
+long_description = ""
 requirements = ["curlify", "bs4", "push_receiver"]
 
 test_requirements = [
