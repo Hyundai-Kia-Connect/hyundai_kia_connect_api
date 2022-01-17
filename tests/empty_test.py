463a17e4dda0b2ff01a69c43411a152da4eb49ca
cdnninja <cdnninja@users.noreply.github.com>
2022-01-17 02:47:43 +0000

    fix: black action

--- tests/empty_test.py
+++ tests/empty_test.py
@@ -1,10 +1,14 @@
 import os
 from hyundai_kia_connect_api import *
+
+
 def login():
-    username = os.environ['KIA_CA_CDNNINJA_USERNAME']
-    password = os.environ['KIA_CA_CDNNINJA_PASSWORD']
-    pin = os.environ['KIA_CA_CDNNINJA_PIN']
-    vm = VehicleManager(region=1, brand=2, username=username, password=password, pin=pin)
+    username = os.environ["KIA_CA_CDNNINJA_USERNAME"]
+    password = os.environ["KIA_CA_CDNNINJA_PASSWORD"]
+    pin = os.environ["KIA_CA_CDNNINJA_PIN"]
+    vm = VehicleManager(
+        region=1, brand=2, username=username, password=password, pin=pin
+    )
     print(vm.check_and_refresh_token())
     print(vm.vehicles)
     return True
