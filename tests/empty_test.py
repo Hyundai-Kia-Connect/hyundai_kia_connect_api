c916f631fda4d78ce5a5fc834e20cdd00bede9fc
cdnninja <jaydenaphillips@gmail.com>
2022-01-16 19:46:53 -0700

    Update empty_test.py

--- tests/empty_test.py
+++ tests/empty_test.py
@@ -1,2 +1,10 @@
-def test_empty():
-    pass
+import os
+from hyundai_kia_connect_api import *
+def login():
+    username = os.environ['KIA_CA_CDNNINJA_USERNAME']
+    password = os.environ['KIA_CA_CDNNINJA_PASSWORD']
+    pin = os.environ['KIA_CA_CDNNINJA_PIN']
+    vm = VehicleManager(region=1, brand=2, username=username, password=password, pin=pin)
+    print(vm.check_and_refresh_token())
+    print(vm.vehicles)
+    return True
