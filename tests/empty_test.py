b7c563c0f2c270524bbe224d4454354c8a35bdda
cdnninja <jaydenaphillips@gmail.com>
2022-01-18 22:23:11 -0700

    Update empty_test.py

--- tests/empty_test.py
+++ tests/empty_test.py
@@ -8,7 +8,8 @@ def test_login():
     password = os.environ["KIA_CA_CDNNINJA_PASSWORD"]
     pin = os.environ["KIA_CA_CDNNINJA_PIN"]
     vm = VehicleManager(
-        region=1, brand=2, username=username, password=password, pin=pin
+        region=2, brand=1, username=username, password=password, pin=pin
     )
+    print(vm.check_and_refresh_token())
     print(vm.vehicles)
     assert len(vm.vehicles.keys()) > 0
