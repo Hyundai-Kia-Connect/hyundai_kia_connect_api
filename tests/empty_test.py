d00e9d795f23b890fab8878af9de77d8d514e236
cdnninja <jaydenaphillips@gmail.com>
2022-02-11 19:16:00 -0700

    Update empty_test.py

--- tests/empty_test.py
+++ tests/empty_test.py
@@ -10,6 +10,5 @@ def test_login():
     vm = VehicleManager(
         region=2, brand=1, username=username, password=password, pin=pin
     )
-    print(vm.check_and_refresh_token())
-    print(vm.vehicles)
+    vm.check_and_refresh_token()
     assert len(vm.vehicles.keys()) > 0
