c12aa7c760df797a1e46660add00d7969500aa9a
cdnninja <jaydenaphillips@gmail.com>
2022-02-13 09:33:12 -0700

    tests: Separate files for tests (#50)
    Co-authored-by: cdnninja <cdnninja@users.noreply.github.com>

--- tests/eu_test_login.py
+++ tests/eu_test_login.py
@@ -3,17 +3,6 @@ import os
 from hyundai_kia_connect_api.VehicleManager import VehicleManager
 
 
-def test_CA_login():
-    username = os.environ["KIA_CA_CDNNINJA_USERNAME"]
-    password = os.environ["KIA_CA_CDNNINJA_PASSWORD"]
-    pin = os.environ["KIA_CA_CDNNINJA_PIN"]
-    vm = VehicleManager(
-        region=2, brand=1, username=username, password=password, pin=pin
-    )
-    vm.check_and_refresh_token()
-    assert len(vm.vehicles.keys()) > 0
-
-
 def test_EU_login():
     username = os.environ["KIA_EU_FUATAKGUN_USERNAME"]
     password = os.environ["KIA_EU_FUATAKGUN_PASSWORD"]
