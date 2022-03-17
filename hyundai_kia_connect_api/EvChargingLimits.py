50daf772793e4944e3d8858ba4cb54ce36fe352e
Julian Wefers <secret@secret>
2022-03-17 14:15:52 +0100

    refactor: make wrapper class a bit nicer

--- hyundai_kia_connect_api/EvChargingLimits.py
+++ hyundai_kia_connect_api/EvChargingLimits.py
@@ -5,9 +5,6 @@ from .const import *
 _LOGGER = logging.getLogger(__name__)
 
 class EvChargingLimits:
-    _dc_charging_limit: int
-    _ac_charging_limit: int
-
     def __init__(self, dc_charging_limit: int, ac_charging_limit: int):
         self.dc_charging_limit = dc_charging_limit
         self.ac_charging_limit = ac_charging_limit
@@ -17,7 +14,7 @@ class EvChargingLimits:
         return self._dc_charging_limit
 
     @dc_charging_limit.setter
-    def dc_charging_limit(self, value) -> int:
+    def dc_charging_limit(self, value: int):
         if value < 50 or value > 100 or value % 10 != 0:
             raise ValueError("Charging limit must be between 50 and 100 and divisable by 10.")
         self._dc_charging_limit = value
@@ -27,7 +24,7 @@ class EvChargingLimits:
         return self._ac_charging_limit
 
     @ac_charging_limit.setter
-    def ac_charging_limit(self, value) -> int:
+    def ac_charging_limit(self, value: int):
         if value < 50 or value > 100 or value % 10 != 0:
             raise ValueError("Charging limit must be between 50 and 100 and divisable by 10.")
         self._ac_charging_limit = value
