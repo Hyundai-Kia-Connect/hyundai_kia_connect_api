5dac407653756dd6071a74a63644fc1a82f68392
cdnninja <jaydenaphillips@gmail.com>
2022-11-11 15:53:56 -0700

    Update VehicleManager.py

--- hyundai_kia_connect_api/VehicleManager.py
+++ hyundai_kia_connect_api/VehicleManager.py
@@ -61,7 +61,7 @@ class VehicleManager:
 
     def update_vehicle_with_cached_state(self, vehicle: Vehicle) -> None:
         self.api.update_vehicle_with_cached_state(self.token, vehicle)
-        if self.geocode_api_enable == True
+        if self.geocode_api_enable == True:
             self.api.update_geocoded_location(self.token, vehicle, geocode_api_use_email)
 
     def check_and_force_update_vehicles(self, force_refresh_interval: int) -> None:
