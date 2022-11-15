0cd8c2b1bae15f748c7cca5202be194b136da029
cdnninja <jaydenaphillips@gmail.com>
2022-11-15 15:16:07 -0700

    Update utils.py

--- hyundai_kia_connect_api/utils.py
+++ hyundai_kia_connect_api/utils.py
@@ -11,12 +11,18 @@ def get_child_value(data, key):
     return value
 
 def get_hex_temp_into_index(value):
-    value = value.replace("H", "")
-    value = int(value, 16)
-    return value
+    if value is not None:
+        value = value.replace("H", "")
+        value = int(value, 16)
+        return value
+    else
+        return None
 
 def get_index_into_hex_temp(value):
-    value = hex(value).split("x")
-    value = value[1] + "H"
-    value = value.zfill(3).upper()
-    return value
+    if value is not None:
+        value = hex(value).split("x")
+        value = value[1] + "H"
+        value = value.zfill(3).upper()
+        return value
+    else
+        return None
