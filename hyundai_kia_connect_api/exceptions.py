23c1ed2174cec591b1b64fa6bc64c7b7745bebc3
Youri Le Cottier <youri.lcd@gmail.com>
2022-11-22 22:14:41 +0100

    (Kia EU) API Error handling + refactor (#142)
    * feat:  Custom Exceptions
    
    * Update KiaUvoApiEU.py
    
    * Update KiaUvoApiEU.py
    
    * - Add API exceptions
    - Perform API error handling
    - Refactor: simplify headers
    - Linting
    
    * make get_authenticated_headers private
    
    Co-authored-by: cdnninja <jaydenaphillips@gmail.com>
    Co-authored-by: zulufoxtrot <youri@tbc-france.com>

---
+++ hyundai_kia_connect_api/exceptions.py
@@ -0,0 +1,53 @@
+class HyundaiKiaException(Exception):
+    """Generic hyundaiKiaException exception.
+    Attributes:
+        stage: the stage that the exception occurred at
+    """
+
+    def __init__(self, stage: str) -> None:
+        self.stage = stage
+
+    pass
+
+
+class AuthenticationError(HyundaiKiaException):
+    """
+    Raised upon receipt of an authentication error.
+    """
+    pass
+
+
+class APIError(HyundaiKiaException):
+    """
+    Generic API error
+    """
+    pass
+
+
+class RateLimitingError(APIError):
+    """
+    Raised when we get rate limited by the server
+    """
+    pass
+
+
+class DuplicateRequestError(APIError):
+    """
+    Raised when (supposedly) a previous request is already queued server-side and the server temporarily rejects
+    requests.
+    """
+    pass
+
+
+class RequestTimeoutError(APIError):
+    """
+    Raised when (supposedly) the server fails to establish a connection with the car.
+    """
+    pass
+
+
+class InvalidAPIResponseError(APIError):
+    """
+    Raised upon receipt of an invalid API response.
+    """
+    pass
