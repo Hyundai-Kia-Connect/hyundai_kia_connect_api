6b881f435d30a836be3d2c181a8e4d4971efb587
zulufoxtrot <youri@tbc-france.com>
2022-11-22 21:37:18 +0100

    - Add API exceptions - Perform API error handling - Refactor: simplify headers - Linting

--- hyundai_kia_connect_api/exceptions.py
+++ hyundai_kia_connect_api/exceptions.py
@@ -8,18 +8,46 @@ class HyundaiKiaException(Exception):
         self.stage = stage
 
     pass
-  
-  
+
+
 class AuthenticationError(HyundaiKiaException):
     """
-    Raised upon receipt of an authentication error. 
+    Raised upon receipt of an authentication error.
     """
-
     pass
-  
-class InvalidAPIResponseError(HyundaiKiaException):
-    """
-    Raised upon receipt of an authentication error. 
-    """
 
+
+class APIError(HyundaiKiaException):
+    """
+    Generic API error
+    """
+    pass
+
+
+class RateLimitingError(APIError):
+    """
+    Raised when we get rate limited by the server
+    """
+    pass
+
+
+class DuplicateRequestError(APIError):
+    """
+    Raised when (supposedly) a previous request is already queued server-side and the server temporarily rejects
+    requests.
+    """
+    pass
+
+
+class RequestTimeoutError(APIError):
+    """
+    Raised when (supposedly) the server fails to establish a connection with the car.
+    """
+    pass
+
+
+class InvalidAPIResponseError(APIError):
+    """
+    Raised upon receipt of an invalid API response.
+    """
     pass
