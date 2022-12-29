ffd9671f6f64d494caf40d75b222b1b4f635b96a
GitHub Action <action@github.com>
2022-12-29 23:14:25 +0000

    chore: bumping version to 1.52.12

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/fuatakgun/hyundai_kia_connect_api",
-    version="1.52.11",
+    version="1.52.12",
     zip_safe=False,
 )
