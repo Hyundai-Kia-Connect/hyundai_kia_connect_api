7801dd3abf7fe5ad416fe3cab1b88b4583c82409
cdnninja <jaydenaphillips@gmail.com>
2023-01-02 19:21:59 -0700

    refactor: flake8 fixes

--- hyundai_kia_connect_api/utils.py
+++ hyundai_kia_connect_api/utils.py
@@ -10,6 +10,7 @@ def get_child_value(data, key):
                 value = None
     return value
 
+
 def get_hex_temp_into_index(value):
     if value is not None:
         value = value.replace("H", "")
@@ -17,6 +18,7 @@ def get_hex_temp_into_index(value):
         return value
     else:
         return None
+
 
 def get_index_into_hex_temp(value):
     if value is not None:
