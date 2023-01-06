c05b10e5419bea8014f0d6a70ce93bcb4e6d359a
cdnninja <jaydenaphillips@gmail.com>
2023-01-06 12:58:08 -0700

    feat(CA): Auth Error Exceptions (#246)
    Co-authored-by: cdnninja <cdnninja@users.noreply.github.com>

--- hyundai_kia_connect_api/KiaUvoApiCA.py
+++ hyundai_kia_connect_api/KiaUvoApiCA.py
@@ -21,6 +21,7 @@ from .const import (
     SEAT_STATUS,
     ENGINE_TYPES,
 )
+from .exceptions import *
 from .Token import Token
 from .utils import (
     get_child_value,
@@ -62,16 +63,39 @@ class KiaUvoApiCA(ApiImpl):
             "sec-fetch-site": "same-origin",
         }
 
+    def _check_response_for_errors(self, response: dict) -> None:
+        """
+        Checks for errors in the API response. If an error is found, an exception is raised.
+        retCode known values:
+        - S: success
+        - F: failure
+        resCode / resMsg known values:
+        - 0000: no error
+        - 7404: "Wrong Username and password"
+        :param response: the API's JSON response
+        """
+
+        error_code_mapping = {
+            "7404": AuthenticationError,
+        }
+        if response["responseHeader"]["responseCode"] == 1:
+            if response["error"]["errorCode"] in error_code_mapping:
+                raise error_code_mapping[response["error"]["errorCode"]](
+                    response["error"]["errorDesc"]
+                )
+            else:
+                raise APIError(f"Server returned: '{response['error']['errorDesc']}'")
+
     def login(self, username: str, password: str) -> Token:
 
         # Sign In with Email and Password and Get Authorization Code
-
         url = self.API_URL + "lgn"
         data = {"loginId": username, "password": password}
         headers = self.API_HEADERS
         response = requests.post(url, json=data, headers=headers)
         _LOGGER.debug(f"{DOMAIN} - Sign In Response {response.text}")
         response = response.json()
+        self._check_response_for_errors(response)
         response = response["result"]
         access_token = response["accessToken"]
         refresh_token = response["refreshToken"]
