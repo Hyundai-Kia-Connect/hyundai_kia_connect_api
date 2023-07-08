609496a38af4786450490f619061152f000e3533
cdnninja <jaydenaphillips@gmail.com>
2023-07-07 18:12:37 -0600

    Update exceptions.py

--- hyundai_kia_connect_api/exceptions.py
+++ hyundai_kia_connect_api/exceptions.py
@@ -17,6 +17,14 @@ class AuthenticationError(HyundaiKiaException):
 
     pass
 
+class DeviceIDError(AuthenticationError):
+    """
+    Raised upon receipt of an Invalid Device ID error.
+    """
+
+    pass
+
+
 
 class APIError(HyundaiKiaException):
     """
