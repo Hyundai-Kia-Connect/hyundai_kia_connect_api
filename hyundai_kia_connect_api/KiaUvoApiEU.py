d99943d0109dc53cfde7ed3e1a5c29d25a4d1e75
cdnninja <jaydenaphillips@gmail.com>
2023-07-07 18:06:09 -0600

    Update KiaUvoApiEU.py

--- hyundai_kia_connect_api/KiaUvoApiEU.py
+++ hyundai_kia_connect_api/KiaUvoApiEU.py
@@ -88,7 +88,7 @@ def _check_response_for_errors(response: dict) -> None:
     - 5091: "Exceeds number of requests"
     - 5921: "No Data Found v2 - No Data Found v2"
     - 9999: "Undefined Error - Response timeout"
-    - Unknown:  "Invalid request body - invalid deviceId", relogin oftend required.
+    - 4002:  "Invalid request body - invalid deviceId", relogin oftend required.
     :param response: the API's JSON response
     """
 
