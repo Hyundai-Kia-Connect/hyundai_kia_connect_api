494935d3db84532029f1eccd2d65a8e0133745b7
GitHub Action <action@github.com>
2023-08-11 03:49:31 +0000

    chore: bumping version to 3.8.1

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/fuatakgun/hyundai_kia_connect_api",
-    version="3.8.0",
+    version="3.8.1",
     zip_safe=False,
 )
