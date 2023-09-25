4c31692e0f6f8327682160cb948a82eb7596be59
pkarimov <pkarimov@users.noreply.github.com>
2023-09-25 17:53:33 +0000

    fix: black action

--- hyundai_kia_connect_api/VehicleManager.py
+++ hyundai_kia_connect_api/VehicleManager.py
@@ -244,7 +244,9 @@ class VehicleManager:
             return KiaUvoApiCA(region, brand, language)
         elif REGIONS[region] == REGION_EUROPE:
             return KiaUvoApiEU(region, brand, language)
-        elif REGIONS[region] == REGION_USA and (BRANDS[brand] == BRAND_HYUNDAI or BRANDS[brand] == BRAND_GENESIS):
+        elif REGIONS[region] == REGION_USA and (
+            BRANDS[brand] == BRAND_HYUNDAI or BRANDS[brand] == BRAND_GENESIS
+        ):
             return HyundaiBlueLinkAPIUSA(region, brand, language)
         elif REGIONS[region] == REGION_USA and BRANDS[brand] == BRAND_KIA:
             return KiaUvoAPIUSA(region, brand, language)
