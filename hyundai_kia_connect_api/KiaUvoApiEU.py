0579e00183354fcdf948132ecffdd9291619ca8e
Bubo08 <Bubo08@users.noreply.github.com>
2024-01-31 20:14:26 +0100

    fix(EU): Additional ccs2 fixes (#490)
    Add the forgotten vehicle.py change and fixed the back window heater
    attribute.
    
    ---------
    
    Co-authored-by: cdnninja <jaydenaphillips@gmail.com>

--- hyundai_kia_connect_api/KiaUvoApiEU.py
+++ hyundai_kia_connect_api/KiaUvoApiEU.py
@@ -420,7 +420,12 @@ class KiaUvoApiEU(ApiImpl):
         elif steer_wheel_heat == 1:
             vehicle.steering_wheel_heater_is_on = True
 
-        # TODO: status.sideBackWindowHeat
+        defrost_rear_is_on = get_child_value(state, "Body.Windshield.Rear.Defog.State")
+        if defrost_rear_is_on in [0, 2]:
+            vehicle.back_window_heater_is_on = False
+        elif defrost_rear_is_on == 1:
+            vehicle.back_window_heater_is_on = True
+
         # TODO: status.sideMirrorHeat
 
         vehicle.front_left_seat_status = SEAT_STATUS[
