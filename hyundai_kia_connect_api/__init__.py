5592c8f448ab957fd1e0328496452e4a875331eb
asychow <asychow@gmail.com>
2024-05-02 16:34:15 +0100

    feat(EU): schedule charging and climate (#548)

--- hyundai_kia_connect_api/__init__.py
+++ hyundai_kia_connect_api/__init__.py
@@ -1,7 +1,12 @@
 """Top-level package for Hyundai / Kia Connect."""
 
 # flake8: noqa
-from .ApiImpl import ApiImpl, ClimateRequestOptions, WindowRequestOptions
+from .ApiImpl import (
+    ApiImpl,
+    ClimateRequestOptions,
+    WindowRequestOptions,
+    ScheduleChargingClimateRequestOptions,
+)
 from .ApiImplType1 import ApiImplType1
 from .HyundaiBlueLinkAPIUSA import HyundaiBlueLinkAPIUSA
 from .KiaUvoApiCA import KiaUvoApiCA
