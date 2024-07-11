a4aa8da373ca5f6489e56ce16e60ba54fdef6f09
hurricane7719 <155265974+hurricane7719@users.noreply.github.com>
2024-07-11 15:38:08 -0400

    Update KiaUvoApiCA.py

--- hyundai_kia_connect_api/KiaUvoApiCA.py
+++ hyundai_kia_connect_api/KiaUvoApiCA.py
@@ -1,6 +1,7 @@
 """KiaUvoApiCA.py"""
 
 # pylint:disable=unused-argument,missing-timeout,logging-fstring-interpolation,bare-except,invalid-name,missing-function-docstring
+# Location Fix
 
 import time
 import datetime as dt
