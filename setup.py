40400a2137fe851f9d860c1efb50c11a177c18b1
GitHub Action <action@github.com>
2024-11-03 13:44:58 +0000

    chore: bumping version to 3.24.3

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/Hyundai-Kia-Connect/hyundai_kia_connect_api",
-    version="3.24.2",
+    version="3.24.3",
     zip_safe=False,
 )
