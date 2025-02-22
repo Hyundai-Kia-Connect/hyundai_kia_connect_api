6d08a2f49284bcd7b79cff015e22de7f2e4e9641
cdnninja <jaydenaphillips@gmail.com>
2025-02-21 19:36:32 -0700

    fix(CA): Safely get model

--- hyundai_kia_connect_api/KiaUvoApiCA.py
+++ hyundai_kia_connect_api/KiaUvoApiCA.py
@@ -172,7 +172,7 @@ class KiaUvoApiCA(ApiImpl):
             vehicle: Vehicle = Vehicle(
                 id=entry["vehicleId"],
                 name=entry["nickName"],
-                model=entry["modelName"],
+                model=entry.get(["modelName"]),
                 year=int(entry["modelYear"]),
                 VIN=entry["vin"],
                 engine_type=entry_engine_type,
