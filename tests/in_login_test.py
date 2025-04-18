aa92f37715b87ddf36e3e000558265f1c3b13878
nishant713 <nishantkumar713@gmail.com>
2025-04-18 17:23:54 +0530

    Update in_login_test.py

--- tests/in_login_test.py
+++ tests/in_login_test.py
@@ -4,9 +4,9 @@ from hyundai_kia_connect_api.VehicleManager import VehicleManager
 
 
 def test_IN_login():
-    username = os.environ["USERNAME"]
-    password = os.environ["PASSWORD"]
-    pin = os.environ["PIN"]
+    username = os.environ["KIA_IN_NISHANT_USERNAME"]
+    password = os.environ["KIA_IN_NISHANT_PASSWORD"]
+    pin = os.environ["KIA_IN_NISHANT_PIN"]
     vm = VehicleManager(
         region=6, brand=2, username=username, password=password, pin=pin
     )
