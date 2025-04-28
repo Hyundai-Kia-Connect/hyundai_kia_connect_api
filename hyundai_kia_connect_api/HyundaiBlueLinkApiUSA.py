e145d51ac3eb972aa1da4a753309bb0ff4be9d52
cdnninja <jaydenaphillips@gmail.com>
2025-04-28 12:15:49 -0600

    fix(USA Hyundai):  EV Heat Start climate (#798)

--- hyundai_kia_connect_api/HyundaiBlueLinkApiUSA.py
+++ hyundai_kia_connect_api/HyundaiBlueLinkApiUSA.py
@@ -811,9 +811,16 @@ class HyundaiBlueLinkApiUSA(ApiImpl):
         if vehicle.engine_type == ENGINE_TYPES.EV:
             data = {
                 "airCtrl": int(options.climate),
+                "igniOnDuration": options.duration,
                 "airTemp": {"value": str(options.set_temp), "unit": 1},
                 "defrost": options.defrost,
                 "heating1": int(options.heating),
+                "seatHeaterVentInfo": {
+                    "drvSeatHeatState": options.front_left_seat,
+                    "astSeatHeatState": options.front_right_seat,
+                    "rlSeatHeatState": options.rear_left_seat,
+                    "rrSeatHeatState": options.rear_right_seat,
+                },
             }
         else:
             data = {
