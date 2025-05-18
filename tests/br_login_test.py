e354a3caaaae95780f0ae0182a211778e2a4e463
vanessa <vanessa.freitasb@gmail.com>
2025-05-18 15:52:36 -0300

    Refactor BR login test; add pytest marker and env checks

--- tests/br_login_test.py
+++ tests/br_login_test.py
@@ -1,21 +1,43 @@
 from hyundai_kia_connect_api import VehicleManager
 import os
+import pytest
 
 from hyundai_kia_connect_api.const import Brand, Region
+
+# To test this, create a .env file with the following variables:
+#
+# BLUELINK_BR_USERNAME=your_username
+# BLUELINK_BR_PASSWORD=your_password
+# BLUELINK_BR_PIN=your_pin
+#
+# pytest will automatically load the .env file.
+#
+# Then, run the tests with:
+# pytest -m br
 
 username = os.getenv("BLUELINK_BR_USERNAME")
 password = os.getenv("BLUELINK_BR_PASSWORD")
 pin = os.getenv("BLUELINK_BR_PIN")
 
 
-def test_brazil_login_hyundai():
-    vm = VehicleManager(
-        region=Region.BRAZIL,
-        brand=Brand.HYUNDAI,
-        username=username,
-        password=password,
-        pin=pin,
-    )
+@pytest.mark.br
+class TestBrazilHyundaiAPI:
+    @classmethod
+    def setup_class(self):
+        if not username or not password or not pin:
+            raise ValueError(
+                "BLUELINK_BR_USERNAME, BLUELINK_BR_PASSWORD, and BLUELINK_BR_PIN must be set to run this test file."
+            )
 
-    vm.check_and_refresh_token()
-    breakpoint()
+    def test_login_hyundai(self):
+        vm = VehicleManager(
+            region=Region.BRAZIL,
+            brand=Brand.HYUNDAI,
+            username=username,
+            password=password,
+            pin=pin,
+        )
+
+        vm.check_and_refresh_token()
+        vm.update_all_vehicles_with_cached_state()
+        assert len(vm.vehicles.keys()) > 0
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "br: mark test for the Brazilian API")
