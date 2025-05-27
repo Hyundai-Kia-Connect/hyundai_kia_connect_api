c9f1fa2519190c1be1f17d1e2c03dbb4eb700fd0
Yasmmin Claudino <vanessa.freitasb@gmail.com>
2025-05-27 12:07:58 -0300

    Explicitly state default opts

--- runtests.py
+++ runtests.py
@@ -1,10 +1,14 @@
-# /env/bin/python
+# !/env/bin/python
 
 import pytest
 import sys
+
+DEFAULT_OPTS = ["-vv"]
 
 # Add the project root to the Python path
 # so absolute imports work
 sys.path.append(".")
 
-pytest.main()
+print("Running pytest with default options:", DEFAULT_OPTS)
+
+pytest.main(args=DEFAULT_OPTS)
