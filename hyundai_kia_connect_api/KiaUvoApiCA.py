29e6728c6034cc66907e71d13b43ca926381d21a
cdnninja <jaydenaphillips@gmail.com>
2025-07-09 21:37:56 -0600

    fix(CA): Gas mode range

--- hyundai_kia_connect_api/KiaUvoApiCA.py
+++ hyundai_kia_connect_api/KiaUvoApiCA.py
@@ -402,10 +402,18 @@ class KiaUvoApiCA(ApiImpl):
         vehicle.fuel_driving_range = (
             get_child_value(
                 state,
+                "status.evStatus.drvDistance.0.rangeByFuel.gasModeRange.value",
+            ),
+            DISTANCE_UNITS[get_child_value(state, "status.evStatus.drvDistance.0.rangeByFuel.gasModeRange.unit")],
+        )
+        if vehicle.fuel_driving_range is None:
+            get_child_value(
+                state,
                 "status.dte.value",
             ),
             DISTANCE_UNITS[get_child_value(state, "status.dte.unit")],
         )
+            
         vehicle.fuel_level_is_low = get_child_value(state, "status.lowFuelLight")
         vehicle.fuel_level = get_child_value(state, "status.fuelLevel")
         vehicle.air_control_is_on = get_child_value(state, "status.airCtrlOn")
