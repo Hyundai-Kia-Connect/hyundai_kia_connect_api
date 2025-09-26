816d7ef3c6f51a2160ec29f46100206b4405c526
robotfishe <64608682+robotfishe@users.noreply.github.com>
2025-09-26 12:55:47 -0600

    Update KiaUvoApiEU.py

--- hyundai_kia_connect_api/KiaUvoApiEU.py
+++ hyundai_kia_connect_api/KiaUvoApiEU.py
@@ -433,12 +433,9 @@ class KiaUvoApiEU(ApiImplType1):
         elif ev_charge_port_door_is_open == 2:
             vehicle.ev_charge_port_door_is_open = False
 
-        if get_child_value(
-            state, "vehicleStatus.evStatus.batteryPower.batteryStndChrgPower"
-        ):
-            vehicle.ev_charging_power = get_child_value(
-                state, "vehicleStatus.evStatus.batteryPower.batteryStndChrgPower"
-            )
+        battery_stnd_chrg_power = get_child_value(state, "vehicleStatus.evStatus.batteryPower.batteryStndChrgPower")
+        if battery_stnd_chrg_power is not None:
+            vehicle.ev_charging_power = battery_stnd_chrg_power
 
         if (
             get_child_value(
