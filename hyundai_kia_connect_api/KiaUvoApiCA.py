Hyundai-Kia-Connect/hyundai_kia_connect_api

commit 753076d62607628d6e144b12afbb72f4fe820440

fix: Remove external dependencies (curlify, pytz, dateutil), fix Canadian timezone detection (#896)
Summary:
* Remove external dependencies (curlify, pytz, dateutil) to reduce
security risks of supply chain attacks.
* Improve Canadian timezone detection, updates #187 by
@torbjornaxelsson.

Details:

* Remove external pytz dependency

pytz notes that "Projects using Python 3.9 or later should be using the
support now included as part of the standard library". Indeed, we can
replace `pytz.utc` by `datetime.timezone.utc`. Its alias `datetime.UTC`
    requires Python 3.11, so we stick to the former for now.

    Automated import deletions and API rewrites with:

        sed -e '/import pytz/,+1{/^\(import pytz\)\?$/d}' \
            -e 's/pytz\.utc/dt.timezone.utc/' \
            $(git grep -l pytz.utc) -i
        ruff format

* Fix Canada/Newfoundland time zone support

    Canadian users can configure a timezone in their car to one of the
Canadian timezones, it does not have to support arbitary others. It also
turns out that Newfoundland is an odd timezone with half an hour diff.

    Refactor the code to check for one of the known timezones instead of
inventing a dummy "VEHICLETIME" one. This allows dropping a dependency
    on tzoffset from the external dateutil library.

Drop unused `self.vehicle_timezone`, removed since commit c885a81f2ae6.

* Remove external dateutil dependency

Use zoneinfo for time zone information, added in Python 3.9. This relies
on the system time zone database whereas dateutil ships one by itself.

    Automated API and import rewrites with:

        sed -e 's/tz.gettz/ZoneInfo/' \
-e 's/from dateutil import tz/from zoneinfo import ZoneInfo/' \
            $(git grep -l tz.gettz) -i

* Remove unused curlify dependency, remove requirements_dev.txt

Removed curlify, a comment to this got removed in commit b861e2d164d2.
Add requests dependency, this was indirectly pulled in by curlify, but
    it is actually a direct dependency.

hyundai_kia_connect_api/KiaUvoApiCA.py
@@ -6,10 +6,8 @@ import time
 import datetime as dt
 import json
 import logging
-import pytz
 import requests
-
-from dateutil.tz import tzoffset
+from zoneinfo import ZoneInfo
 
 from .ApiImpl import ApiImpl, ClimateRequestOptions
 from .Token import Token
@@ -34,6 +32,7 @@ from .utils import (
     get_hex_temp_into_index,
     get_index_into_hex_temp,
     parse_datetime,
+    detect_timezone_for_date,
 )
 
 
@@ -65,6 +64,12 @@ firefox = [
 ]
 
 _LOGGER = logging.getLogger(__name__)
+
+
+CA_TIMEZONES = [
+    ZoneInfo(f"Canada/{zone}")
+    for zone in "Newfoundland Atlantic Eastern Central Mountain Pacific".split()
+]
 
 
 # Use the custom cipher order
@@ -121,7 +126,6 @@ class KiaUvoApiCA(ApiImpl):
     temperature_range_model_year = 2020
 
     def __init__(self, region: int, brand: int, language: str) -> None:
-        self.vehicle_timezone = self.data_timezone
         self.LANGUAGE: str = language
         self.brand = brand
         if BRANDS[brand] == BRAND_KIA:
@@ -198,7 +202,9 @@ class KiaUvoApiCA(ApiImpl):
         access_token = response["accessToken"]
         refresh_token = response["refreshToken"]
 
-        valid_until = dt.datetime.now(pytz.utc) + dt.timedelta(seconds=token_expire_in)
+        valid_until = dt.datetime.now(dt.timezone.utc) + dt.timedelta(
+            seconds=token_expire_in
+        )
 
         return Token(
             username=username,
@@ -282,18 +288,21 @@ class KiaUvoApiCA(ApiImpl):
     def force_refresh_vehicle_state(self, token: Token, vehicle: Vehicle) -> None:
         state = self._get_forced_vehicle_state(token, vehicle)
 
-        # Calculate offset between vehicle last_updated_at and UTC
-        self.vehicle_timezone = vehicle.timezone
+        # lastStatusDate uses one of the Canadian timezones configured through
+        # the car entertainment system.
         last_updated_at = parse_datetime(
-            get_child_value(state, "status.lastStatusDate"), self.data_timezone
+            get_child_value(state, "status.lastStatusDate"), dt.timezone.utc
         )
-        now_utc: dt = dt.datetime.now(pytz.utc)
-        offset = round((last_updated_at - now_utc).total_seconds() / 3600)
-        _LOGGER.debug(f"{DOMAIN} - Offset between vehicle and UTC: {offset} hours")
-        if offset != 0:
-            # Set our timezone to account for the offset
-            vehicle.timezone = tzoffset("VEHICLETIME", offset * 3600)
-            _LOGGER.debug(f"{DOMAIN} - Set vehicle.timezone to UTC + {offset} hours")
+        ref_date = dt.datetime.now()
+        tz = detect_timezone_for_date(last_updated_at, ref_date, CA_TIMEZONES)
+        if tz:
+            _LOGGER.debug(f"{DOMAIN} - Set vehicle.timezone to {tz} (guessed)")
+            vehicle.timezone = tz
+        else:
+            delta = (ref_date - last_updated_at).total_seconds() / 3600
+            _LOGGER.warning(
+                f"{DOMAIN} - could not guess Canadian timezone! delta is {delta} hours"
+            )
 
         self._update_vehicle_properties_base(vehicle, state)
 
@@ -320,7 +329,6 @@ class KiaUvoApiCA(ApiImpl):
 
     def _update_vehicle_properties_base(self, vehicle: Vehicle, state: dict) -> None:
         _LOGGER.debug(f"{DOMAIN} - Old Vehicle Last Updated: {vehicle.last_updated_at}")
-        self.vehicle_timezone = vehicle.timezone
         vehicle.last_updated_at = parse_datetime(
             get_child_value(state, "status.lastStatusDate"), self.data_timezone
         )
@@ -524,7 +532,6 @@ class KiaUvoApiCA(ApiImpl):
         self, vehicle: Vehicle, state: dict
     ) -> None:
         if get_child_value(state, "coord.lat"):
-            self.vehicle_timezone = vehicle.timezone
             vehicle.location = (
                 get_child_value(state, "coord.lat"),
                 get_child_value(state, "coord.lon"),

hyundai_kia_connect_api/HyundaiBlueLinkApiUSA.py
@@ -5,7 +5,6 @@
 import logging
 import time
 import datetime as dt
-import pytz
 import requests
 import certifi
 
@@ -66,7 +65,7 @@ class HyundaiBlueLinkApiUSA(ApiImpl):
     """HyundaiBlueLinkApiUSA"""
 
     # initialize with a timestamp which will allow the first fetch to occur
-    last_loc_timestamp = dt.datetime.now(pytz.utc) - dt.timedelta(hours=3)
+    last_loc_timestamp = dt.datetime.now(dt.timezone.utc) - dt.timedelta(hours=3)
 
     def __init__(self, region: int, brand: int, language: str):
         self.LANGUAGE: str = language
@@ -137,7 +136,9 @@ class HyundaiBlueLinkApiUSA(ApiImpl):
         refresh_token = response["refresh_token"]
         expires_in = float(response["expires_in"])
 
-        valid_until = dt.datetime.now(pytz.utc) + dt.timedelta(seconds=expires_in)
+        valid_until = dt.datetime.now(dt.timezone.utc) + dt.timedelta(
+            seconds=expires_in
+        )
 
         return Token(
             username=username,

hyundai_kia_connect_api/KiaUvoApiAU.py
@@ -9,9 +9,8 @@ import random
 import uuid
 from urllib.parse import parse_qs, urlparse
 
-import pytz
 import requests
-from dateutil import tz
+from zoneinfo import ZoneInfo
 
 from .ApiImplType1 import ApiImplType1
 from .Token import Token
@@ -54,7 +53,7 @@ USER_AGENT_MOZILLA: str = "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build
 
 
 class KiaUvoApiAU(ApiImplType1):
-    data_timezone = tz.gettz("Australia/Sydney")
+    data_timezone = ZoneInfo("Australia/Sydney")
     temperature_range = [x * 0.5 for x in range(34, 54)]
 
     def __init__(self, region: int, brand: int, language: str) -> None:
@@ -109,7 +108,7 @@ class KiaUvoApiAU(ApiImplType1):
             authorization_code, stamp
         )
         _, refresh_token = self._get_refresh_token(authorization_code, stamp)
-        valid_until = dt.datetime.now(pytz.utc) + dt.timedelta(hours=23)
+        valid_until = dt.datetime.now(dt.timezone.utc) + dt.timedelta(hours=23)
 
         return Token(
             username=username,

hyundai_kia_connect_api/KiaUvoApiCN.py
@@ -10,9 +10,8 @@ from typing import Optional
 from time import sleep
 from urllib.parse import parse_qs, urlparse
 
-import pytz
 import requests
-from dateutil import tz
+from zoneinfo import ZoneInfo
 
 from .ApiImpl import (
     ClimateRequestOptions,
@@ -105,7 +104,7 @@ def _check_response_for_errors(response: dict) -> None:
 
 
 class KiaUvoApiCN(ApiImplType1):
-    data_timezone = tz.gettz("Asia/Shanghai")
+    data_timezone = ZoneInfo("Asia/Shanghai")
     temperature_range = [x * 0.5 for x in range(28, 60)]
 
     def __init__(self, region: int, brand: int, language: str) -> None:
@@ -174,7 +173,7 @@ class KiaUvoApiCN(ApiImplType1):
 
         _, access_token, authorization_code = self._get_access_token(authorization_code)
         _, refresh_token = self._get_refresh_token(authorization_code)
-        valid_until = dt.datetime.now(pytz.utc) + LOGIN_TOKEN_LIFETIME
+        valid_until = dt.datetime.now(dt.timezone.utc) + LOGIN_TOKEN_LIFETIME
 
         return Token(
             username=username,

hyundai_kia_connect_api/KiaUvoApiEU.py
@@ -10,10 +10,9 @@ import uuid
 import re
 from urllib.parse import parse_qs, urlparse
 
-import pytz
 import requests
 from bs4 import BeautifulSoup
-from dateutil import tz
+from zoneinfo import ZoneInfo
 
 
 from .ApiImplType1 import ApiImplType1
@@ -76,7 +75,7 @@ SUPPORTED_LANGUAGES_LIST = [
 
 
 class KiaUvoApiEU(ApiImplType1):
-    data_timezone = tz.gettz("Europe/Berlin")
+    data_timezone = ZoneInfo("Europe/Berlin")
     temperature_range = [x * 0.5 for x in range(28, 60)]
 
     def __init__(self, region: int, brand: int, language: str) -> None:
@@ -184,7 +183,9 @@ class KiaUvoApiEU(ApiImplType1):
             _, access_token, authorization_code, expires_in = self._get_access_token(
                 stamp, refresh_token
             )
-            valid_until = dt.datetime.now(pytz.utc) + dt.timedelta(seconds=expires_in)
+            valid_until = dt.datetime.now(dt.timezone.utc) + dt.timedelta(
+                seconds=expires_in
+            )
 
             return Token(
                 username=username,
@@ -215,7 +216,9 @@ class KiaUvoApiEU(ApiImplType1):
             _, access_token, authorization_code, expires_in = self._get_access_token(
                 stamp, authorization_code
             )
-            valid_until = dt.datetime.now(pytz.utc) + dt.timedelta(seconds=expires_in)
+            valid_until = dt.datetime.now(dt.timezone.utc) + dt.timedelta(
+                seconds=expires_in
+            )
 
             _, refresh_token = self._get_refresh_token(stamp, authorization_code)
 

hyundai_kia_connect_api/KiaUvoApiIN.py
@@ -11,9 +11,8 @@ import re
 import math
 from urllib.parse import parse_qs, urlparse
 from typing import Optional
-import pytz
 import requests
-from dateutil import tz
+from zoneinfo import ZoneInfo
 from .ApiImpl import (
     ClimateRequestOptions,
 )
@@ -78,7 +77,7 @@ SUPPORTED_LANGUAGES_LIST = [
 
 
 class KiaUvoApiIN(ApiImplType1):
-    data_timezone = tz.gettz("Asia/Kolkata")
+    data_timezone = ZoneInfo("Asia/Kolkata")
     temperature_range = [x * 0.5 for x in range(28, 60)]
 
     def __init__(self, brand: int) -> None:
@@ -150,7 +149,7 @@ class KiaUvoApiIN(ApiImplType1):
             stamp, authorization_code
         )
         _, refresh_token = self._get_refresh_token(stamp, authorization_code)
-        valid_until = dt.datetime.now(pytz.utc) + dt.timedelta(hours=23)
+        valid_until = dt.datetime.now(dt.timezone.utc) + dt.timedelta(hours=23)
 
         return Token(
             username=username,

hyundai_kia_connect_api/KiaUvoApiUSA.py
@@ -12,7 +12,6 @@ import typing
 from datetime import datetime
 
 import certifi
-import pytz
 import requests
 from requests import RequestException, Response
 from requests.adapters import HTTPAdapter
@@ -153,7 +152,7 @@ class KiaUvoApiUSA(ApiImpl):
         }
         # Should produce something like "Mon, 18 Oct 2021 07:06:26 GMT".
         # May require adjusting locale to en_US
-        date = datetime.now(tz=pytz.utc).strftime("%a, %d %b %Y %H:%M:%S GMT")
+        date = datetime.now(tz=dt.timezone.utc).strftime("%a, %d %b %Y %H:%M:%S GMT")
         headers["date"] = date
         headers["deviceid"] = self.device_id
         return headers
@@ -199,7 +198,7 @@ class KiaUvoApiUSA(ApiImpl):
                 f"{DOMAIN} - No session id returned in login. Response: {response.text} headers {response.headers} cookies {response.cookies}"  # noqa
             )
         _LOGGER.debug(f"got session id {session_id}")
-        valid_until = dt.datetime.now(pytz.utc) + LOGIN_TOKEN_LIFETIME
+        valid_until = dt.datetime.now(dt.timezone.utc) + LOGIN_TOKEN_LIFETIME
         return Token(
             username=username,
             password=password,

hyundai_kia_connect_api/VehicleManager.py
@@ -5,8 +5,6 @@
 import datetime as dt
 import logging
 
-import pytz
-
 from .exceptions import APIError
 from .ApiImpl import (
     ApiImpl,
@@ -116,7 +114,7 @@ class VehicleManager:
     ) -> None:
         # Force refresh only if current data is older than the value bassed in seconds.
         # Otherwise runs a cached update.
-        started_at_utc: dt = dt.datetime.now(pytz.utc)
+        started_at_utc: dt.datetime = dt.datetime.now(dt.timezone.utc)
         vehicle = self.get_vehicle(vehicle_id)
         if vehicle.last_updated_at is not None:
             _LOGGER.debug(
@@ -146,7 +144,7 @@ class VehicleManager:
         if self.token is None:
             self.initialize()
         if (
-            self.token.valid_until <= dt.datetime.now(pytz.utc)
+            self.token.valid_until <= dt.datetime.now(dt.timezone.utc)
             or self.api.test_token(self.token) is False
         ):
             _LOGGER.debug(f"{DOMAIN} - Refresh token expired")

commit 3b18e7beb83c7ae1ef0e029d6f1c7bb641a000c8

fix: restore explicit timezone for Canadian responses
Fixes a regression introduced with a previous Canadian timezone fix,
`datetime.now()` requires an explicit timezone parameter to prevent:

      File "hyundai_kia_connect_api/utils.py", line 107, in detect_timezone_for_date
        delta = (ref_date - date.replace(tzinfo=tz)).total_seconds()
                 ~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~
    TypeError: can't subtract offset-naive and offset-aware datetimes

Link: https://github.com/Hyundai-Kia-Connect/kia_uvo/issues/1316#issuecomment-3341841390
Fixes: 753076d62607

hyundai_kia_connect_api/KiaUvoApiCA.py
@@ -293,7 +293,7 @@ class KiaUvoApiCA(ApiImpl):
         last_updated_at = parse_datetime(
             get_child_value(state, "status.lastStatusDate"), dt.timezone.utc
         )
-        ref_date = dt.datetime.now()
+        ref_date = dt.datetime.now(tz=dt.timezone.utc)
         tz = detect_timezone_for_date(last_updated_at, ref_date, CA_TIMEZONES)
         if tz:
             _LOGGER.debug(f"{DOMAIN} - Set vehicle.timezone to {tz} (guessed)")