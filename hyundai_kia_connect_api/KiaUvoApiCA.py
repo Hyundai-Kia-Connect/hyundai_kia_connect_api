00c3a4edbd8dcbf6bc3da6c9fed1f6bc5e061063
Bogdan Cornea <corneabogdan@gmail.com>
2025-12-03 15:30:33 -0500

    fix: Force IPv4 cleanly using urllib3 allowed_gai_family

--- hyundai_kia_connect_api/KiaUvoApiCA.py
+++ hyundai_kia_connect_api/KiaUvoApiCA.py
@@ -9,6 +9,14 @@ import logging
 import requests
 from zoneinfo import ZoneInfo
 
+import socket
+import requests.packages.urllib3.util.connection as urllib3_cn
+
+def allowed_gai_family():
+    return socket.AF_INET
+
+urllib3_cn.allowed_gai_family = allowed_gai_family
+
 from .ApiImpl import ApiImpl, ClimateRequestOptions
 from .Token import Token
 from .Vehicle import Vehicle, DailyDrivingStats
