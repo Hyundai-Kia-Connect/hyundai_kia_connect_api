3b848e131b3d89bf331daf47ac2483cf3ab6a8ee
cdnninja <jaydenaphillips@gmail.com>
2026-01-12 20:03:00 -0700

    feat: token to dict

--- hyundai_kia_connect_api/Token.py
+++ hyundai_kia_connect_api/Token.py
@@ -3,7 +3,7 @@
 # pylint:disable=invalid-name
 
 import datetime as dt
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 
 
 @dataclass
@@ -19,3 +19,31 @@ class Token:
     valid_until: dt.datetime = dt.datetime.min
     stamp: str = None
     pin: str = None
+
+    def to_dict(self) -> dict:
+        """Convert Token to a JSON‑serializable dict."""
+        data = asdict(self)
+
+        # Convert datetime to ISO string
+        data["valid_until"] = self.valid_until.isoformat()
+
+        return data
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "Token":
+        """Create a Token instance from a dict."""
+        # Parse datetime from ISO string
+        valid_until = data.get("valid_until")
+        if isinstance(valid_until, str):
+            valid_until = dt.datetime.fromisoformat(valid_until)
+
+        return cls(
+            username=data.get("username"),
+            password=data.get("password"),
+            access_token=data.get("access_token"),
+            refresh_token=data.get("refresh_token"),
+            device_id=data.get("device_id"),
+            valid_until=valid_until,
+            stamp=data.get("stamp"),
+            pin=data.get("pin"),
+        )
