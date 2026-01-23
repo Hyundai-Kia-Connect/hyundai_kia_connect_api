7fcef0ad8f614aa2bde69c3d6538f7ae767bee87
cdnninja <jaydenaphillips@gmail.com>
2026-01-23 14:39:23 -0700

    feat: add V2H

--- hyundai_kia_connect_api/HyundaiBlueLinkApiUSA.py
+++ hyundai_kia_connect_api/HyundaiBlueLinkApiUSA.py
@@ -436,6 +436,12 @@ class HyundaiBlueLinkApiUSA(ApiImpl):
             get_child_value(state, "vehicleStatus.evStatus.remainTime2.etc3.value"),
             "m",
         )
+        vehicle.ev_v2h_status = get_child_value(
+            state, "lastVehicleInfo.vehicleStatusRpt.vehicleStatus.evStatus.v2hStatus"
+        )
+        vehicle.ev_v2l_status = get_child_value(
+            state, "lastVehicleInfo.vehicleStatusRpt.vehicleStatus.evStatus.v2lStatus"
+        )
         if get_child_value(
             state,
             "vehicleStatus.evStatus.drvDistance.0.rangeByFuel.gasModeRange.value",
